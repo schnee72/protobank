0b2a80297100dd2e4cd9a9aba0dbb303d52996d9
Dustin Schnee <schnee72@gmail.com>
2018-08-07 21:15:07 -0500

    throttle

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 * auth
 * api calls
 * nav ease bottom
-* mobile - hover?
 * sass variables
 * dark / light themes
 * settings &#9881;
 * async actions
 * persist state
+* versioning
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+// TODO - subcribe to store - persist in localstorage..
+// TODO - use throttle so it only saves the state 1/sec at most
+// TODO - consider splitting it so auth state is stored in sessionstorage
+
 new Vue({
   router,
   store,
